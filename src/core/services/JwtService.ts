59fbfb4fc63c2a37ef8910eb8d6f4f848d899c33
gys316 <gilyunseo316@gmail.com>
2024-01-04 14:51:19 +0900

    decode 오류 수정

--- src/core/services/JwtService.ts
+++ src/core/services/JwtService.ts
@@ -23,13 +23,17 @@ export const destroyToken = (): void => {
 };
 
 export const decode = (token) => {
-  var base64Url = token.split('.')[1];
-  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-  }).join(''));
+  try {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
 
-  return JSON.parse(jsonPayload);
+    return JSON.parse(jsonPayload);
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 export default { getToken, saveToken, destroyToken, decode };
